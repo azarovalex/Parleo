0c74e1aae54a1f9e3aa6be288a7ec269086ae2e0
Azarov Alex <alex@azarov.by>
2019-02-22 22:12:33 +0300

    Initial Commit

---
+++ Parleo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Parleo
+//
+//  Created by Alex Azarov on 2/22/19.
+//  Copyright © 2019 LeatherSoft. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
