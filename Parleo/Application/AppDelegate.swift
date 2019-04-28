9529fcfed1ad873bef0bd4484bfae9f2a3b0c2c8
Azarov Alex <alex@azarov.by>
2019-04-28 19:02:15 +0300

    Push notifications

--- Parleo/Application/AppDelegate.swift
+++ Parleo/Application/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import RxSwiftExt
 import IQKeyboardManagerSwift
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,7 +20,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         setupNavigationButtons()
         IQKeyboardManager.shared.enable = true
 //        Storage.shared.accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFsZXhAYXphcm92LmJ5IiwianRpIjoiM2YwYThkMDgtNzY5Yy00NzA3LWIxZTQtZmJmOGVjZjdjNDhjIiwiZXhwIjoxNTU4OTY2NjI1fQ.QFCK0UjBxXejWAh4jAz-1qjvB-DftANon4sXA4eM7b0"
+        registerForPushNotifications()
         return true
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print(deviceToken.reduce("") { $0 + String(format: "%02x", $1) })
+    }
+
+    func registerForPushNotifications() {
+        UNUserNotificationCenter.current()
+            .requestAuthorization(options: [.alert, .sound, .badge]) { [weak self] granted, _ in
+                guard granted else { return }
+                self?.getNotificationSettings()
+        }
+    }
+
+    func getNotificationSettings() {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            guard settings.authorizationStatus == .authorized else { return }
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
     }
 }
 
